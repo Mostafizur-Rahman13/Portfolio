25bcbe8e07434106e068b2aa5d15e219e54a5d2b
Mostafizur-Rahman13 <mostafiztarek@gmail.com>
2024-08-19 23:50:23 +0600

    Updated Projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
 import project1 from "../assets/projects/Project-1.png";
 import project2 from "../assets/projects/Project-2.png";
 import project3 from "../assets/projects/Project-3.png";
-// import project4 from "../assets/projects/Project-4.png";
 
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
@@ -14,42 +13,36 @@ export const EXPERIENCES = [
     role: "Frontend Developer",
     company: "Unisoft Technology",
     description: `Designed and developed user interfaces for web applications using JavaScript,Next.js and React. Worked closely with backend developers to integrated with MongoDB database and frontend components with JavaScript, Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Tailwind CSS", "Bootstrap 5", "JavaScript", "Next.JS", "React.JS",],
+    technologies: ["HTML", "CSS", "Tailwind CSS", "Bootstrap 5", "JavaScript", "Nextjs", "Reactjs",],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Cloud Kitchen App",
+    image: project3,
+    deploy: "http://mostafiz-cloud-kitchen-nextjs.vercel.app",
+    description:
+      "A Next.js-based platform for cloud kitchens, enhancing efficiency and scalability.",
+    technologies: ["HTML", "CSS", "Tailwind", "Nextjs"]
+  },
+  {
+    title: "Colth E-Commerce App",
     image: project1,
     deploy: "https://mostafiz-cloth-tailwind-reactjs.netlify.app",
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React.JS", "Node.JS", "MongoDB", "Socket.io"],
+    technologies: ["HTML", "Tailwind", "Reactjs", "Nodejs", "MongoDB", "Socket.io"],
   },
   {
-    title: "Task Management App",
+    title: "Foods Order & Delivery App",
     image: project2,
     deploy: "https://mostafiz-foods-app.netlify.app",
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "Tailwind CSS", "React", "Node.JS", "MongoDB"],
+      "A web platform enabling users to explore, select, and purchase meals from various restaurants efficiently online.",
+    technologies: ["HTML", "CSS", "Reactjs", "Nodejs", "MongoDB"],
   },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React.JS", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project1,
-    deploy: "",
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+
 ];
 
 export const CONTACT = {
